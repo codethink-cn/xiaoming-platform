958728b8dd3b395937376eddf0f5346195df5df1
Chuanwise <chuanwise@qq.com>
2023-02-12 20:42:30 +0800

    add Subject interface

---
+++ xiaoming-platform-core-api/src/main/java/cn/codethink/xiaoming/platform/plugin/Plugin.java
@@ -0,0 +1,12 @@
+package cn.codethink.xiaoming.platform.plugin;
+
+import cn.codethink.xiaoming.platform.subject.Subject;
+
+/**
+ * <h1>插件</h1>
+ *
+ * @author Chuanwise
+ */
+public interface Plugin
+    extends Subject {
+}
